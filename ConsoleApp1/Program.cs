c8dc36c133951b72b689381af9b1f012d3f6f79d
closhara <et.alexander.n@eduiteh.eu>
2025-09-29 11:10:14 +0300

    leap year

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿
+string input = Console.ReadLine();
+int year = int.Parse(input);
+
+bool condition1 = (year % 4) == 0;
+bool condition2 = (year % 100) != 0;
+bool condition3 = (year % 400) == 0;
+if ((condition1 && condition2) || condition3)
+    Console.WriteLine("Leap year");
+else
+    Console.WriteLine("Normal year");
