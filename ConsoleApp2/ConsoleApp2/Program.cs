3dd22d3a8a3b2b1b2039dc457014b8cb4e98936e
stef <et.stefan.s@eduiteh.eu>
2025-10-13 10:33:34 +0300

    none

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string day4Input = Console.ReadLine();
+            double num = double.Parse(day4Input);
+            Console.WriteLine(num);
+
+            day4Input = Console.ReadLine();
+            num = double.Parse(day4Input);
+            Console.WriteLine(num);
+        }
+    }
+}
