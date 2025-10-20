8ade49663b49e46c264d2b8afae844c1d339ab44
etstefans-commits <et.stefan.s@eduiteh.eu>
2025-10-20 11:23:01 +0300

    20.10.2025 task

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,30 @@
+﻿//int counter = 0;
+//while (counter < 10)
+//{
+//    Console.WriteLine("text");
+//    counter++;
+//}
+
+//for (int interator = 0; interator < 10; interator++)
+//{
+//    Console.WriteLine(interator);
+//}
+
+string input = Console.ReadLine();
+int n = int.Parse(input);
+int sum = 0;
+int product = 1;
+for (int i = 0; i < n; ++i)
+{
+    sum += i;
+    product *= i;
+    Console.WriteLine("Sum: " + i.ToString() + " " + sum.ToString());
+    Console.WriteLine("Product: " + i.ToString() + " " + sum.ToString());
+}
+Console.WriteLine(sum);
+Console.WriteLine(product);
+
+for (int i = 1; i <= n; ++i)
+{
+    Console.WriteLine(i);
+}
