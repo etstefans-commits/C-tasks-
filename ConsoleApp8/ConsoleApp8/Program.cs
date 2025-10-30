2c1929a2c5459f03d0107f16d8b90d3147365547
etstefans-commits <et.stefan.s@eduiteh.eu>
2025-10-30 11:21:01 +0200

    30.10.2025 task

---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Input a number: ");
+int num = int.Parse(Console.ReadLine());
+int max = int.MinValue;
+Console.WriteLine("Enter " + num + " numbers:");
+for (int i = 0; i < num; i++)
+{
+    int n = int.Parse(Console.ReadLine());
+    if (n > max)
+    {
+        max = n;
+    }
+}
+Console.WriteLine("The biggest number is: " + max);
