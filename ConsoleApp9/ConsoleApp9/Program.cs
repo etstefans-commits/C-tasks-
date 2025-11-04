9162651fff4920289cb5879909de21703724c4bc
etstefans-commits <et.stefan.s@eduiteh.eu>
2025-11-04 11:20:52 +0200

    Create Program.cs

---
+++ ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,17 @@
+﻿//int num = int.Parse(Console.ReadLine());
+//while (num >= 10)
+//{
+//    Console.WriteLine(num % 10);
+//    num = num / 10;
+//}
+//Console.WriteLine(num);
+
+int primeNumber = int.Parse(Console.ReadLine());
+for (int i = 2;  i < primeNumber; i++)
+{
+    if (primeNumber % i == 0)
+    {
+        Console.WriteLine(i + " is not a prime number");
+        break;
+    }
+}
